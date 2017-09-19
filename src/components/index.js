79e7f288521dfcff3251404254119ce9bcce18b0
Devesh Kaushal <dkaushal2@sapient.com>
2017-09-19 17:22:31 +0530

    added new react component

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+// Demo component
+// this is only example component
+// you can find tests in __test__ folder
+
+import React from 'react';
+
+class MyComponent2 extends React.Component {
+  
+    render() {
+        return (
+            <div>I am npm package react component2</div>
+        )
+    }
+};
+
+export default MyComponent2;
