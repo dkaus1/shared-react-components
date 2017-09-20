26778cd06bb3e786d19a3fdc3f3ff8fa0acfad36
Devesh Kaushal <dkaushal2@sapient.com>
2017-09-20 23:17:02 +0530

    added typescript component

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
 	HeaderComponent :require('./dist/index').default,
 	MessagingComponent : require('./dist/components/index').default,
-	DataComponent: require('./dist/BiDirectional/index').default
+	DataComponent: require('./dist/BiDirectional/index').default,
+	TypescriptComponent: require('./dist/components/greeter').default
 	} ;
---
+++ src/components/greeter.tsx
@@ -0,0 +1,16 @@
+// Demo component
+// this is only example component
+// you can find tests in __test__ folder
+
+import * as React from 'react';
+
+class TypescriptComponent extends React.Component {
+  
+    render() {
+        return (
+            <div>I am npm package react TypescriptComponent</div>
+        )
+    }
+};
+
+export default TypescriptComponent;
