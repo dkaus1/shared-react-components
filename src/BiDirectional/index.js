1bde3379e39a5b526c662675c7ca77d6e05e6e53
Devesh Kaushal <dkaushal2@sapient.com>
2017-09-20 18:27:26 +0530

    updated to es6 bidirectional component

--- src/BiDirectional/index.js
+++ src/BiDirectional/index.js
@@ -1,22 +1,21 @@
-var React = require('react');
+import React from 'react';
+class BiDirectionalComponent extends React.Component {
+	
+	render() {
+		var _props = this.props;
+		var html = _props.todos.map(function(item, i) {
+		  //pass item as parameter in the click method passing item.text doesnt work, it returns undefined
+		  return <li key={i} onClick={(event)=>{_props.callReact(item)}}>{item.text}</li>
+		});
+		return (
+		  <div>
+			<ul>
+			  {html}
+			</ul>
+			<button onClick={()=>{this.props.alertCallFromReact('message from React Component to Angular Directive')}}>Call from Component to Directive</button>
+		  </div>
+		);
+	  }
+  };
 
-var BiDirectional = React.createClass({
-
-  render() {
-    var _props = this.props;
-    var html = _props.todos.map(function(item, i) {
-      //pass item as parameter in the click method passing item.text doesnt work, it returns undefined
-      return <li key={i} onClick={(event)=>{_props.callReact(item)}}>{item.text}</li>
-    });
-    return (
-      <div>
-        <ul>
-          {html}
-        </ul>
-        <button onClick={()=>{this.props.alertCallFromReact('message from React Component to Angular Directive')}}>Call from Component to Directive</button>
-      </div>
-    );
-  }
-});
-
-module.exports = BiDirectional;
+  export default BiDirectionalComponent;
